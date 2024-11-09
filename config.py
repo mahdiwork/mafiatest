1740f3a5388d5e4d54d15124bbca8d4f58e7f3c9
mahdiwork <165424460+mahdiwork@users.noreply.github.com>
2024-11-09 23:25:30 +0330

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ LogFile = 'mafia.log'
 Local = True
 #config={'user': os.environ['database_user'], 'password': os.environ['database_password'], 'host': 'localhost','database':'detabase_property'}
 # config={'user': 'root', 'password': 'ma8h2dii', 'host': 'localhost','database':'detabase_mafia'}
-config={'user': 'root', 'password': 'ka25wWJnMY4ilhXrDoLl', 'host': 'localhost','database':'datacvq_db'}
+config={'user': 'root', 'password': 'ka25wWJnMY4ilhXrDoLl', 'host': 'data-jjg-service','database':'datacvq_db'}
 
